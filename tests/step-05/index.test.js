0d93a4896e9fb654e4c6400afb10dbd51cbd2da5
souvik-maity <souvik1252@gmail.com>
2024-04-08 02:47:17 +0530

    feat:step 8

--- tests/step-05/index.test.js
+++ tests/step-05/index.test.js
@@ -3,25 +3,27 @@ const parseQuery = require('../../src/queryParser');
 const executeSELECTQuery = require('../../src/index');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
+    const data = await readCSV('./student.csv'); // Update file path to student.csv
     expect(data.length).toBeGreaterThan(0);
     expect(data.length).toBe(3);
     expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+    expect(data[0].age).toBe('30'); // Ignore the string type here, we will fix this later
 });
 
 test('Parse SQL Query', () => {
-    const query = 'SELECT id, name FROM sample';
+    const query = 'SELECT id, name FROM student'; // Update table name to student
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
-        table: 'sample',
+        table: 'student', // Update table name to student
+        joinCondition: null,
+      joinTable: null,
         whereClauses: [],
     });
 });
 
 test('Execute SQL Query', async () => {
-    const query = 'SELECT id, name FROM sample';
+    const query = 'SELECT id, name FROM student'; // Update table name to student
     const result = await executeSELECTQuery(query);
     expect(result.length).toBeGreaterThan(0);
     expect(result[0]).toHaveProperty('id');
@@ -31,23 +33,25 @@ test('Execute SQL Query', async () => {
 });
 
 test('Parse SQL Query with WHERE Clause', () => {
-    const query = 'SELECT id, name FROM sample WHERE age = 25';
+    const query = 'SELECT id, name FROM student WHERE age = 25'; // Update table name to student
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
-        table: 'sample',
+        joinCondition: null,
+      joinTable: null,
+        table: 'student', // Update table name to student
         whereClauses:  [
-                  {
-                   field: "age",
-                   operator:"=",
-                   value: "25",
-                 },
-               ],
+            {
+                field: "age",
+                operator:"=",
+                value: "25",
+            },
+        ],
     });
 });
 
 test('Execute SQL Query with WHERE Clause', async () => {
-    const query = 'SELECT id, name FROM sample WHERE age = 25';
+    const query = 'SELECT id, name FROM student WHERE age = 25'; // Update table name to student
     const result = await executeSELECTQuery(query);
     expect(result.length).toBe(1);
     expect(result[0]).toHaveProperty('id');
