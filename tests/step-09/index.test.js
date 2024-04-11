93d502fb534d979290a4f09b56c7af951d74acf2
souvik-maity <souvik1252@gmail.com>
2024-04-11 22:25:24 +0530

    feat-step-9

--- tests/step-09/index.test.js
+++ tests/step-09/index.test.js
@@ -5,7 +5,7 @@ const executeSELECTQuery = require('../../src/index');
 test('Read CSV File', async () => {
     const data = await readCSV('./student.csv'); // Update file path to student.csv
     expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(3);
+    expect(data.length).toBe(4);
     expect(data[0].name).toBe('John');
     expect(data[0].age).toBe('30'); // Ignore the string type here, we will fix this later
 });
@@ -87,14 +87,14 @@ test('Execute SQL Query with Multiple WHERE Clause', async () => {
 test('Execute SQL Query with Greater Than', async () => {
     const queryWithGT = 'SELECT id FROM student WHERE age > 22'; // Update table name to student
     const result = await executeSELECTQuery(queryWithGT);
-    expect(result.length).toEqual(2);
+    expect(result.length).toEqual(3);
     expect(result[0]).toHaveProperty('id');
 });
 
 test('Execute SQL Query with Not Equal to', async () => {
     const queryWithGT = 'SELECT name FROM student WHERE age != 25'; // Update table name to student
     const result = await executeSELECTQuery(queryWithGT);
-    expect(result.length).toEqual(2);
+    expect(result.length).toEqual(3);
     expect(result[0]).toHaveProperty('name');
 });
 test('Parse SQL Query with INNER JOIN', async () => {
